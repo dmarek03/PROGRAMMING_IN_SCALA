65ee62907649247f61ce8ca546f89a63af29617d
dmarek03 <domarek@student.agh.edu.pl>
2024-12-02 21:32:24 +0100

    Added lab_02

--- lab_02/src/main/scala/lab43.scala
+++
@@ -1,8 +0,0 @@
-def sumArrayRec3(elems: Array[Int]): Int =
-    def goFrom(i: Int, size: Int, elms: Array[Int]): Int =
-        if (i < size) elms(i) + goFrom(i + 1, size, elms)
-        else 0
-    goFrom(0, elems.length, elems)
-
-@main def ex43_01(): Unit =
-    println("sumArrayRec3 = " + sumArrayRec3((0 until 30000).toArray))
